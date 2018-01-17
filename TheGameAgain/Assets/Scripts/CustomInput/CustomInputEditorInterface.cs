d8d5053a51f083634e1bb038bba747774af93fd6
Juha-Matti <juhamk91@gmail.com>
2018-01-17 18:26:58 +0200

    Added controller button images for various controller types. Also made CustomInputManager that reads project settings inputmanager axises. The can be changed using CustomInputManager and CustomInputEditorInterface in a same scene. Note. CustomInputEditorInterface runs in the editor mode and it has the interface to 	add, save and load axis in Unitys own InputManager. Basically, I made the missing interface to edit it. 	Only, it is by changing the actual axis settings save file.

---
+++ TheGameAgain/Assets/Scripts/CustomInput/CustomInputEditorInterface.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheGameAgain
+{
+    [ExecuteInEditMode]
+    public class CustomInputEditorInterface : MonoBehaviour
+    {
+        [Header("Editor only script")]
+        [SerializeField]
+        private CustomInputManager inputManager;
+
+        [SerializeField]
+        CustomInputManager.Axis axis = new CustomInputManager.Axis();
+
+        [Header("Buttons")]
+        public Boolean addAxis;
+        public Boolean clear;
+        public Boolean defaultInputs;
+        public Boolean minimumInputs;
+        
+
+        [Header("File")]
+        public String fileName = "InputManager.asset";
+        public Boolean loadInputs;
+        public Boolean saveInputs;
+
+        public void Update()
+        {
+            if (addAxis) { AddAxis(); }
+            if (clear) { ClearAxis(); }
+            if (defaultInputs) { DefaultInputs(); }
+            if (minimumInputs) { MinimumInputs(); }
+            if (saveInputs) { SaveInputs(); }
+            if (loadInputs) { LoadInputs(); }
+        }
+
+        private void AddAxis()
+        {
+            inputManager.axes.Add(axis);
+            axis = new CustomInputManager.Axis();
+            addAxis = false;
+        }
+
+        private void ClearAxis()
+        {
+            axis = new CustomInputManager.Axis();
+            clear = false;
+        }
+
+        private void DefaultInputs()
+        {
+            //Clear axes
+            inputManager.axes.Clear();
+
+            //Key inputs
+            CustomInputManager.Axis axis = inputManager.AddAxis("Horizontal");
+            axis.button.positive = "right";
+            axis.button.negative = "left";
+            axis.altButton.positive = "d";
+            axis.altButton.negative = "a";
+            axis.snap = true;
+            axis.gravity = 3f;
+            axis.sensitivity = 3f;
+
+            axis = inputManager.AddAxis("Vertical");
+            axis.button.positive = "up";
+            axis.button.negative = "down";
+            axis.altButton.positive = "w";
+            axis.altButton.negative = "s";
+            axis.snap = true;
+            axis.gravity = 3f;
+            axis.sensitivity = 3f;
+
+            axis = inputManager.AddAxis("Fire1");
+            axis.button.positive = "left ctrl";
+            axis.altButton.positive = "mouse 0";
+
+            axis = inputManager.AddAxis("Fire2");
+            axis.button.positive = "left alt";
+            axis.altButton.positive = "mouse 1";
+
+            axis = inputManager.AddAxis("Fire3");
+            axis.button.positive = "left shift";
+            axis.altButton.positive = "mouse 2";
+
+            axis = inputManager.AddAxis("Jump");
+            axis.button.positive = "space";
+
+            //Mouse inputs
+            axis = inputManager.AddAxis("Mouse X");
+            axis.gravity = 0f;
+            axis.deadzone = 0f;
+            axis.sensitivity = 0.1f;
+            axis.type = InputType.Mouse_Movement;
+            axis.axis = InputAxis.X_axis;
+
+            axis = inputManager.AddAxis("Mouse Y");
+            axis.gravity = 0f;
+            axis.deadzone = 0f;
+            axis.sensitivity = 0.1f;
+            axis.type = InputType.Mouse_Movement;
+            axis.axis = InputAxis.Y_axis;
+
+            axis = inputManager.AddAxis("Mouse ScrollWheel");
+            axis.gravity = 0f;
+            axis.deadzone = 0f;
+            axis.sensitivity = 0.1f;
+            axis.type = InputType.Mouse_Movement;
+            axis.axis = InputAxis.Z_axis;
+
+            //Joystick inputs
+            axis = inputManager.AddAxis("Horizontal");
+            axis.gravity = 0f;
+            axis.deadzone = 0.19f;
+            axis.sensitivity = 1f;
+            axis.type = InputType.Joystick_Axis;
+            axis.axis = InputAxis.X_axis;
+            axis.joystick = 0;
+
+            axis = inputManager.AddAxis("Vertical");
+            axis.gravity = 0f;
+            axis.deadzone = 0.19f;
+            axis.sensitivity = 1f;
+            axis.invert = true;
+            axis.type = InputType.Joystick_Axis;
+            axis.axis = InputAxis.Y_axis;
+            axis.joystick = 0;
+
+            axis = inputManager.AddAxis("Fire1");
+            axis.button.positive = "joystick button 0";
+
+            axis = inputManager.AddAxis("Fire2");
+            axis.button.positive = "joystick button 1";
+
+            axis = inputManager.AddAxis("Fire3");
+            axis.button.positive = "joystick button 2";
+
+            axis = inputManager.AddAxis("Jump");
+            axis.button.positive = "joystick button 3";
+
+            axis = inputManager.AddAxis("Submit");
+            axis.button.positive = "return";
+            axis.altButton.positive = "joystick button 0";
+
+            axis = inputManager.AddAxis("Submit");
+            axis.button.positive = "enter";
+            axis.altButton.positive = "space";
+
+            axis = inputManager.AddAxis("Cancel");
+            axis.button.positive = "escape";
+            axis.altButton.positive = "joystick button 1";
+
+            defaultInputs = false;
+        }
+
+        private void MinimumInputs()
+        {
+            minimumInputs = false;
+        }
+
+        private void LoadInputs()
+        {
+            inputManager.Load(fileName);
+            loadInputs = false;
+        }
+
+        private void SaveInputs()
+        {
+            inputManager.Save(fileName);
+            saveInputs = false;
+        }
+    }
+}
---
+++ TheGameAgain/Assets/Scripts/CustomInput/CustomInputManager.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+namespace TheGameAgain
+{
+    public enum InputType
+    {
+        Key_or_Mouse_Button, Mouse_Movement, Joystick_Axis
+    }
+    //Has 28 different axis support... Z axis is mousewheel or 3rd 
+    public enum InputAxis
+    {
+        X_axis, Y_axis, Z_axis, Axis_4, Axis_5, Axis_6, Axis_7, Axis_8, Axis_9, Axis_10, Axis_11, Axis_12, Axis_13, Axis_14, Axis_15, Axis_16, Axis_17, Axis_18, Axis_19, Axis_20, Axis_21, Axis_22, Axis_23, Axis_24, Axis_25, Axis_26, Axis_27, Axis_28
+    }
+    public class CustomInputManager : MonoBehaviour
+    {
+        [Serializable]
+        public struct YinYang
+        {
+            public string positive, negative;
+
+            public void defaults()
+            {
+                positive = "";
+                negative = "";
+            }
+        }
+        [Serializable]
+        public class Axis
+        {
+            public string name;
+            public YinYang description, button, altButton;
+            public float gravity, deadzone, sensitivity;
+            public bool snap, invert;
+            public InputType type;
+            public InputAxis axis;
+            public int joystick;
+
+            public Axis()
+            {
+                name = "";
+                description = new YinYang();
+                description.defaults();
+                button = new YinYang();
+                button.defaults();
+                altButton = new YinYang();
+                altButton.defaults();
+                gravity = 1000f;
+                deadzone = 0.001f;
+                sensitivity = 1000f;
+                snap = false;
+                invert = false;
+                type = InputType.Key_or_Mouse_Button;
+                axis = InputAxis.X_axis;
+                joystick = 0;
+            }
+        }
+
+        [SerializeField]
+        public List<Axis> axes = new List<Axis>();
+
+        // Use this for initialization
+        void Start()
+        {
+            Load(Application.dataPath + "/../ProjectSettings/InputManager.asset");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public Axis AddAxis(string name)
+        {
+            axes.Add(new Axis());
+            Axis x = axes[axes.Count - 1];
+            x.name = name;
+            return x;
+        }
+
+        public void Save(string filename)
+        {
+            string target = Application.dataPath;
+            if (filename == "InputManager.asset")
+            {
+                target += "/../ProjectSettings/InputManager.asset";
+            }
+            else
+            {
+                target += "/" + Path.GetFileNameWithoutExtension(filename) + ".asset";
+            }
+
+            string start = "%YAML 1.1\n" +
+                "% TAG !u!tag:unity3d.com,2011:\n" +
+                "---!u!13 & 1\n" +
+                "InputManager:\n" +
+                "  m_ObjectHideFlags: 0\n" +
+                "  serializedVersion: 2\n" +
+                "  m_Axes:\n";
+            string content = start;
+            axes.ForEach(delegate (Axis axis) {
+                content += "  - serializedVersion: 3\n" +
+                "    m_Name: " + axis.name + "\n" +
+                "    descriptiveName: " + axis.description.positive + "\n" +
+                "    descriptiveNegativeName: " + axis.description.negative + "\n" +
+                "    negativeButton: " + axis.button.positive + "\n" +
+                "    positiveButton: " + axis.button.negative + "\n" +
+                "    altNegativeButton: " + axis.altButton.positive + "\n" +
+                "    altPositiveButton: " + axis.altButton.negative + "\n" +
+                "    gravity: " + axis.gravity.ToString() + "\n" +
+                "    dead: " + axis.deadzone.ToString() + "\n" +
+                "    sensitivity: " + axis.sensitivity.ToString() + "\n" +
+                "    snap: " + (axis.snap ? 1 : 0) + "\n" +
+                "    invert: " + (axis.invert ? 1 : 0) + "\n" +
+                "    type: " + (int)axis.type + "\n" +
+                "    axis: " + (int)axis.axis + "\n" +
+                "    joyNum: " + axis.joystick + "\n";
+            });
+
+            Write(target, content);
+        }
+
+        private void Write(string target, string content)
+        {
+            Debug.Log("Write method is not implemented yet");
+            File.WriteAllText(target, content);
+        }
+
+        public void Load(string filename)
+        {
+            string target = Application.dataPath;
+            if (filename == "InputManager.asset")
+            {
+                target += "/../ProjectSettings/InputManager.asset";
+            }
+            else
+            {
+                target += "/" + Path.GetFileNameWithoutExtension(filename) + ".asset";
+                if (!File.Exists(target))
+                {
+                    //If file doesn't exist, leave.
+                    Debug.Log("File doesn't exist");
+                    return;
+                }
+            }
+
+            string content = File.ReadAllText(target);
+            //TODO read data to axes list
+            string[] rows = content.Split('\n');
+            axes.Clear();
+            Axis x = null;
+            for(int i = 7, j = 0; (i + j) < rows.Length; j++)
+            {
+                string[] data = rows[i + j].Split(':');
+                if (j % 16 == 0)
+                {
+                    //If these happens, stop reading more
+                    if (data.Length == 0) return; //Nothing to split
+                    if (!data[0].Contains("serializedVersion"))
+                    {
+                        //There's something, but it's not right.
+                        Debug.Log("Wrong line or something... data: " + data.ToString());
+                        return;
+                    }
+                    continue;
+                }
+                if (j % 16 == 1) { x = AddAxis(data[1].Trim()); continue; }
+                if (j % 16 == 2) { x.description.positive = data[1].Trim(); continue; }
+                if (j % 16 == 3) { x.description.negative = data[1].Trim(); continue; }
+                if (j % 16 == 4) { x.button.negative = data[1].Trim(); continue; }
+                if (j % 16 == 5) { x.button.positive = data[1].Trim(); continue; }
+                if (j % 16 == 6) { x.altButton.negative = data[1].Trim(); continue; }
+                if (j % 16 == 7) { x.altButton.positive = data[1].Trim(); continue; }
+                if (j % 16 == 8) { x.gravity = float.Parse(data[1].Trim()); continue; }
+                if (j % 16 == 9) { x.deadzone = float.Parse(data[1].Trim()); continue; }
+                if (j % 16 == 10) { x.sensitivity = float.Parse(data[1].Trim()); continue; }
+                if (j % 16 == 11) { x.snap = int.Parse(data[1].Trim()).Equals(1); continue; }
+                if (j % 16 == 12) { x.invert = int.Parse(data[1].Trim()).Equals(1); continue; }
+                if (j % 16 == 13) { x.type = (InputType)int.Parse(data[1].Trim()); continue; }
+                if (j % 16 == 14) { x.axis = (InputAxis)int.Parse(data[1].Trim()); continue; }
+                if (j % 16 == 15) { x.joystick = int.Parse(data[1].Trim()); continue; }
+            }
+        }
+    }
+}
